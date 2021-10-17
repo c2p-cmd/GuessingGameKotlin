fa9c56203042eb453297b371686ccc30dfbaebd2
Sharan Thakur <60012248+c2p-cmd@users.noreply.github.com>
2021-10-17 17:44:09 +0530

    Added game-icon

--- src/main/kotlin/com/example/demo1/GuessingApp.kt
+++ src/main/kotlin/com/example/demo1/GuessingApp.kt
@@ -3,13 +3,16 @@ package com.example.demo1
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
+import javafx.scene.image.Image
 import javafx.stage.Stage
+import java.util.*
 
 class HelloApplication : Application() {
     private lateinit var controller: HelloController
     override fun start(stage: Stage) {
         val fxmlLoader = FXMLLoader(
-            HelloApplication::class.java.getResource("guessing-game.fxml")
+            HelloApplication::class.java
+                .getResource("guessing-game.fxml")
         )
 
 
@@ -17,6 +20,12 @@ class HelloApplication : Application() {
 
         stage.title = "Guess The Number!!"
         stage.scene = scene
+        stage.icons.add(
+            Image(
+                HelloApplication::class.java
+                    .getResourceAsStream("guess-icon.png")
+            )
+        )
         stage.isResizable = false
         stage.show()
     }
